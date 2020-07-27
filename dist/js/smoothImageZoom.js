9c2b25273d7afe90c62d358e9e248dece72b1589
prakhar-shukla <prakharshuklaofficial@gmail.com>
2020-07-27 15:42:18 +0530

    Resizing support added

--- dist/js/smoothImageZoom.js
+++ dist/js/smoothImageZoom.js
@@ -9,41 +9,50 @@
         window.smoothImageZoom = factory();
     }
 }(function () {
-
-    function zoomUnzoomImage() {
-        if (this.classList.contains('zoomed')) {
+    let imageResizing = false;
+    function zoomUnzoomImage(resizeEvent) {
+        if (!resizeEvent && this.classList.contains('zoomed')) {
             this.classList.remove('zoomed');
             this.style.transform = "";
             document.querySelector('.image-backdrop').classList.remove('zoomed');
             removeZoomOutListeners();
+            removeResizeListener();
         }
         else {
+            let imageCordinates
+            if (resizeEvent) {
+                imageCordinates = this._originalImageCordinates;
+            }
+            else {
+                imageCordinates = getBoundingClientRect(this);
+                this._originalImageCordinates = imageCordinates;
+            }
 
-            var imageCordinates = this.getBoundingClientRect();
+            const deviceRatio = window.innerHeight / window.innerWidth;
+            const imageRatio = imageCordinates.height / imageCordinates.width;
 
-            var isMobileDevice = window.innerHeight > window.innerWidth;
-            var imageScale = isMobileDevice ? window.innerWidth / imageCordinates.width :
+            const imageScale = deviceRatio > imageRatio ?
+                window.innerWidth / imageCordinates.width :
                 window.innerHeight / imageCordinates.height;
 
-            var imageX = ((imageCordinates.left + (imageCordinates.width) / 2));
-            var imageY = ((imageCordinates.top + (imageCordinates.height) / 2));
+            const imageX = ((imageCordinates.left + (imageCordinates.width) / 2));
+            const imageY = ((imageCordinates.top + (imageCordinates.height) / 2));
 
-            var bodyX = (window.innerWidth) / 2;
-            var bodyY = (window.innerHeight) / 2;
+            const bodyX = (window.innerWidth) / 2;
+            const bodyY = (window.innerHeight) / 2;
 
-
-            var xOffset = (bodyX - imageX) / (imageScale);
-            var yOffset = (bodyY - imageY) / (imageScale);
-
+            const xOffset = (bodyX - imageX) / (imageScale);
+            const yOffset = (bodyY - imageY) / (imageScale);
 
             this.style.transform = "scale(" + imageScale + ") translate(" + xOffset + "px," + yOffset + "px) ";
             this.classList.add('zoomed');
             document.querySelector('.image-backdrop').classList.add('zoomed');
-            registersZoomOutListeners();
+            registerZoomOutListeners();
+            registerResizeListener();
         }
     }
 
-    function registersZoomOutListeners() {
+    function registerZoomOutListeners() {
         // zoom out on scroll
         document.addEventListener('scroll', scrollZoomOut);
         // zoom out on escape
@@ -58,8 +67,16 @@
         document.querySelector('.image-backdrop').removeEventListener('click', backDropClickZoomOut);
     }
 
+    function registerResizeListener() {
+        window.addEventListener('resize', onWindowResize)
+    }
+
+    function removeResizeListener() {
+        window.removeEventListener('resize', onWindowResize)
+    }
+
     function scrollZoomOut() {
-        if (document.querySelector('.zoomable-image.zoomed')) {
+        if (document.querySelector('.zoomable-image.zoomed') && !imageResizing) {
             zoomUnzoomImage.call(document.querySelector('.zoomable-image.zoomed'));
         }
     }
@@ -74,9 +91,44 @@
         }
     }
 
+    function onWindowResize() {
+        imageResizing = true;
+        if (document.querySelector('.zoomable-image.zoomed')) {
+            debounce(
+                function () {
+                    zoomUnzoomImage.call(document.querySelector('.zoomable-image.zoomed'), true)
+                    imageResizing = false;
+                }, 100)()
+        }
+    }
+
+    function getBoundingClientRect(element) {
+        var rect = element.getBoundingClientRect();
+        return {
+            top: rect.top,
+            right: rect.right,
+            bottom: rect.bottom,
+            left: rect.left,
+            width: rect.width,
+            height: rect.height,
+            x: rect.x,
+            y: rect.y
+        };
+    }
+    function debounce(func, delay) {
+        let debounceTimer
+        return function () {
+            const context = this
+            const args = arguments
+            clearTimeout(debounceTimer)
+            debounceTimer
+                = setTimeout(() => func.apply(context, args), delay)
+        }
+    }
     function init() {
         document.addEventListener('click', function (event) {
-            if (event && event.target && event.target.className.includes('zoomable-image')) {
+            if (event && event.target && event.target.className &&
+                event.target.className.includes('zoomable-image')) {
                 zoomUnzoomImage.call(event.target)
             }
         });
